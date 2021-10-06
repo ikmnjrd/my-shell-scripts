a59680558044f2944fecf7c6876100351ad084de
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-10-06 23:46:43 +0900

    first commit

---
+++ brew-casks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+brew install iterm2 --cask
+brew install docker --cask
+brew install google-chrome --cask
+brew install visual-studio-code --cask
+brew install firefox --cask
+brew install alfred --cask
+brew install microsoft-edge --cask
+brew install slack --cask
+brew install sublime-text --cask
---
+++ brew-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+brew install awscli
+brew install terraform
+brew install git
