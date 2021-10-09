93299ddf35aa672bd9a128150250f67e87076193
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-10-09 17:42:40 +0900

    add vscode

---
+++ vscode-extension-install.sh
@@ -0,0 +1,6 @@
+code --install-extension coenraads.bracket-pair-colorizer
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension toba.vsfire
+code --install-extension oderwat.indent-rainbow
+code --install-extension ms-ceintl.vscode-language-pack-ja
+code --install-extension dbaeumer.vscode-eslint
