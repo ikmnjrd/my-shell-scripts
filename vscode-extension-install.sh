bc7b8f2d2d5b77fad0931fa12178605cba1f1d45
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-10-11 23:23:53 +0900

    add draw.io extension

--- vscode-extension-install.sh
+++ vscode-extension-install.sh
@@ -5,3 +5,4 @@ code --install-extension toba.vsfire
 code --install-extension oderwat.indent-rainbow
 code --install-extension ms-ceintl.vscode-language-pack-ja
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension hediet.vscode-drawio
