ea4169d3ef6a0980d65a9e99658035bdc99f16b4
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-11-01 00:18:52 +0900

    :sparkles: rust

--- vscode-extension-install.sh
+++ vscode-extension-install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-code --install-extension coenraads.bracket-pair-colorizer
+
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension toba.vsfire
-code --install-extension oderwat.indent-rainbow
-code --install-extension ms-ceintl.vscode-language-pack-ja
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension hediet.vscode-drawio
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension hashicorp.terraform
+## 日本語化
+code --install-extension ms-ceintl.vscode-language-pack-ja
+## 便利ツール
+code --install-extension oderwat.indent-rainbow
+code --install-extension coenraads.bracket-pair-colorizer
+## draw.io
+code --install-extension hediet.vscode-drawio
+## マークダウン
 code --install-extension yzhang.markdown-all-in-one
+## rust関連
+code --install-extension rust-lang.rust
+code --install-extension be5invis.toml
