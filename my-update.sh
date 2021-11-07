6f82c29e938a1fa4c227546978dcfb8f0d6a1fa0
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-11-07 12:10:15 +0900

    add nvm

--- my-update.sh
+++ my-update.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# homebrew
 brew upgrade
 brew upgrade --cask
+# nvm(node version manager) - https://github.com/nvm-sh/nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
