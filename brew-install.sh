2a6f34f2bcd2aa43a4fc04766663e8c7a66d5e33
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-07-10 23:40:45 +0900

    add: fzf & bat & ripgrep

--- brew-install.sh
+++ brew-install.sh
@@ -11,3 +11,8 @@ brew install gnupg
 brew install yarn --ignore-dependencies
 brew install neovim
 brew install pastel # https://github.com/sharkdp/pastel
+brew install fzf #https://github.com/junegunn/fzf
+# To install useful key bindings and fuzzy completion:
+$(brew --prefix)/opt/fzf/install
+brew install bat #https://github.com/sharkdp/bat
+brew install ripgrep #https://github.com/BurntSushi/ripgrep
