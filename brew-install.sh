e45773ee20c1faa909b36f1068b0e57ce06d4bb7
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-10-17 21:14:05 +0900

    add ffmpeg for whisper

--- brew-install.sh
+++ brew-install.sh
@@ -21,6 +21,7 @@ brew install jq #https://github.com/stedolan/jq
 brew install glow #md-viewer https://github.com/charmbracelet/glow
 brew install smug # tmux auto workflow
 brew install gh # GitHub Cli tool https://github.com/cli/cli
+brew install ffmpeg # for whisper https://github.com/openai/whisper
 brew tap daipeihust/tap && brew install im-select #vim insert escape: https://github.com/daipeihust/im-select
 brew install koekeishiya/formulae/yabai
 brew install koekeishiya/formulae/skhd
